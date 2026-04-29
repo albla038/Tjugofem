b7038b1b0947e43572c5c97cd62f43a49c16aeed
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-04-29 16:40:19 +0200

    feat: Add `updateTransactionAction`

--- app/(dashboard)/transactions/actions.ts
+++ app/(dashboard)/transactions/actions.ts
@@ -1,10 +1,12 @@
 "use server";
 
-import { createTransaction } from "@/data/transaction/mutations";
+import { createTransaction, updateTransaction } from "@/data/transaction/mutations";
 import { requireUser } from "@/data/user/verify-user";
 import {
   TransactionCreate,
   transactionCreateSchema,
+  TransactionUpdate,
+  transactionUpdateSchema,
 } from "@/schemas/transaction";
 import { ActionResponse } from "@/types/results";
 import { revalidatePath } from "next/cache";
@@ -24,6 +26,32 @@ export async function createTransactionAction(
 
   // Call DAL mutation
   const mutationRes = await createTransaction(validated.data);
+
+  if (!mutationRes.ok) {
+    return { success: false, errorCode: mutationRes.errorCode };
+  }
+
+  // Revalidate cache
+  revalidatePath("/transactions");
+
+  return { success: true, data: undefined };
+}
+
+export async function updateTransactionAction(
+  data: TransactionUpdate
+): Promise<ActionResponse> {
+  // Authenticate user
+  await requireUser();
+
+  // Validate input data
+  const validated = transactionUpdateSchema.safeParse(data);
+
+  if (!validated.success) {
+    return { success: false, errorCode: "VALIDATION_FAILED" };
+  }
+
+  // Call DAL mutation
+  const mutationRes = await updateTransaction(validated.data);
 
   if (!mutationRes.ok) {
     return { success: false, errorCode: mutationRes.errorCode };
